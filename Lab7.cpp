cfe4f7b154dfc89c14dca871fbf110238d8b0f6d
Kristopher Lowell <kristopher_lowell@student.uml.edu>
2018-08-15 12:22:38 -0400

    Add files via upload

--- Lab7.cpp
+++ Lab7.cpp
@@ -7,34 +7,76 @@ Purpose:
 
 #include <iostream>
 #include <string>
-#include "OnlineSale.h"
-#include "WholesaleStoreSale.h"
-#include "RetailStoreSale.h"
+#include "Sale.h"
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+	OnlineSale OS;
 	WholesaleStoreSale WSS;
+	RetailStoreSale RSS;
 
 	string name;
 	double price;
 
-	cout << "Enter a name for the product: ";
-	cin >> name;
+	cout << "Enter a name for the Online Sale Product: ";
+	getline(cin, name);
+	OS.setProduct(name);
+
+	cout << "Enter a price for the Online Sale product: ";
+	cin >> price;
+	OS.setPrice(price);
+	cin.ignore(INT8_MAX, '\n');
+
+	cout << "Enter the address for the Online Sale: ";
+	getline(cin, name);
+	OS.setAddress(name);
+
+	cout << endl;
+	cout << "Enter a name for the Wholesale Store Sale Product: ";
+	getline(cin, name);
 	WSS.setProduct(name);
 
-	cout << "Enter a price for the product: ";
-	cin >> price;
+	cout << "Enter a price for the Wholesale Store Sale product: ";
+	getline(cin, name);
 	WSS.setPrice(price);
 
 	cout << endl;
+	cout << "Enter a name for the Retail Store Sale Product: ";
+	getline(cin, name);
+	RSS.setProduct(name);
+
+	cout << "Enter a price for the Retail Store Sale product: ";
+	cin >> price;
+	RSS.setPrice(price);
+
+	cout.setf(ios_base::fixed);
+	cout.precision(2);
+
+	cout << endl << "Online Sale:" << endl;
+	cout << "Name: " << OS.getProduct() << endl;
+	cout << "Price: " << OS.getPrice() << endl;
+	cout << "Tax: " << OS.calculateTax() << endl;
+	cout << "Total: " << OS.bill() << endl;
+	cout << endl;
+
+	cout << endl << "Wholesale Store Sale:" << endl;
 	cout << "Name: " << WSS.getProduct() << endl;
 	cout << "Price: " << WSS.getPrice() << endl;
 	cout << "Tax: " << WSS.calculateTax() << endl;
 	cout << "Discounted Price: " << WSS.calculateDiscountedValue() << endl;
-	cout << "Discounted Tax: " << WSS.calculateTax() << endl;
+	cout << "Discounted Tax: " << WSS.calculateDiscountedTax() << endl;
 	cout << "Total: " << WSS.bill() << endl;
+	cout << endl;
+
+	cout << endl << "Retail Store Sale:" << endl;
+	cout << "Name: " << RSS.getProduct() << endl;
+	cout << "Price: " << RSS.getPrice() << endl;
+	cout << "Tax: " << RSS.calculateTax() << endl;
+	cout << "Discounted Price: " << RSS.calculateDiscountedValue() << endl;
+	cout << "Discounted Tax: " << RSS.calculateDiscountedTax() << endl;
+	cout << "Total: " << RSS.bill() << endl;
 	cout << endl;
 
 	system("PAUSE");
--- Sale.h
+++ Sale.h
@@ -21,13 +21,90 @@ public:
 	void setPrice(double priceVal) { price = priceVal; };
 
 	string getProduct() { return product; };
-	double getPrice() { return price; };
+	double getPrice() { return (double) price; };
 
-	virtual double bill() { return price; };
-	virtual double calculateTax(double taxRate) { return price * taxRate; };
+	virtual double bill() = 0;
+	virtual double calculateTax(double taxRate = 0) { return price * taxRate; };
 
 private:
 	string product;
 	double price;
 };
+
+// --------------------------ONLINE SALE --------------------------
+
+class OnlineSale : public Sale
+{
+public:
+	OnlineSale() { taxRate = 0.10; };
+	~OnlineSale() { };
+
+	void setAddress(string addressVal) { address = addressVal; };
+	string getAddress() { return address; };
+
+	double calculateTax() { return this->getPrice() * taxRate; };
+
+	double bill() { return this->getPrice() + this->calculateTax(); };
+
+private:
+	double taxRate;
+	string address;
+};
+
+// -------------------------- STORE SALE --------------------------
+
+class StoreSale : public Sale
+{
+public:
+	StoreSale() { taxRate = 0.20; };
+	~StoreSale() { };
+
+	double getTaxRate() { return taxRate; };
+
+	double calculateTax() { return this->getPrice() * taxRate; };
+
+	double calculateDiscountedValue(double discountValue) { return this->getPrice() * (1.00 - discountValue); };
+
+	virtual double bill() { return this->getPrice() + this->calculateTax(); };
+
+private:
+	double taxRate;
+};
+
+// -------------------------- WHOLESALE STORE SALE --------------------------
+
+class WholesaleStoreSale : public StoreSale
+{
+public:
+	WholesaleStoreSale() { discountValue = 0.25; };
+	~WholesaleStoreSale() {};
+
+	double calculateDiscountedValue() { return this->getPrice() * (1.00 - discountValue); };
+
+	double calculateDiscountedTax() { return this->calculateDiscountedValue() * this->getTaxRate(); };
+
+	double bill() { return calculateDiscountedValue() + calculateDiscountedTax(); };
+
+private:
+	double discountValue;
+};
+
+// -------------------------- RETAIL STORE SALE --------------------------
+
+class RetailStoreSale : public StoreSale
+{
+public:
+	RetailStoreSale() { discountValue = 0.10; };
+	~RetailStoreSale() {};
+
+	double calculateDiscountedValue() { return this->getPrice() * (1.00 - discountValue); };
+
+	double calculateDiscountedTax() { return this->calculateDiscountedValue() * this->getTaxRate(); };
+
+	double bill() { return calculateDiscountedValue() + calculateDiscountedTax(); };
+
+private:
+	double discountValue;
+};
+
 #endif
